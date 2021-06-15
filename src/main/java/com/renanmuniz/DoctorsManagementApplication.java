02496025b8c9783ef99bd4d840c3f3e5748f4e96
Renan Muniz <fmrenan@outlook.com>
2021-06-15 20:38:28 -0300

    Project created

---
+++ src/main/java/com/renanmuniz/DoctorsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.renanmuniz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renanmuniz/DoctorsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanmuniz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
