8b7590f6459afee459bd779812f12b6428e62fa5
Renan Muniz <fmrenan@outlook.com>
2021-06-15 21:33:13 -0300

    Entities, DB Seed

---
+++ src/main/java/com/renanmuniz/backend/entities/Doctor.java
@@ -0,0 +1,126 @@
+package com.renanmuniz.backend.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_doctor")
+public class Doctor implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String crm;
+	private String phone;
+	private String cellPhone;
+	private String cep;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_doctor_specialty",
+		joinColumns = @JoinColumn(name = "doctor_id"),
+		inverseJoinColumns = @JoinColumn(name = "specialty_id"))	
+	private Set<Specialty> specialties =  new HashSet<>();
+	
+	public Doctor() {}
+
+	public Doctor(Long id, String name, String crm, String phone, String cellPhone, String cep) {
+		this.id = id;
+		this.name = name;
+		this.crm = crm;
+		this.phone = phone;
+		this.cellPhone = cellPhone;
+		this.cep = cep;
+	}
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCrm() {
+		return crm;
+	}
+
+	public void setCrm(String crm) {
+		this.crm = crm;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getCellPhone() {
+		return cellPhone;
+	}
+
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public Set<Specialty> getSpecialties() {
+		return specialties;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Doctor other = (Doctor) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	};	
+	
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_specialty (name) VALUES('Alergologia');
+INSERT INTO tb_specialty (name) VALUES('Angiologia');
+INSERT INTO tb_specialty (name) VALUES('Buco maxilo');
+INSERT INTO tb_specialty (name) VALUES('Cardiologia clínca');
+INSERT INTO tb_specialty (name) VALUES('Cardiologia infantil');
+INSERT INTO tb_specialty (name) VALUES('Cirurgia cabeça e pescoço');
+INSERT INTO tb_specialty (name) VALUES('Cirurgia cardíaca');
+INSERT INTO tb_specialty (name) VALUES('Cirurgia de tórax');
