7bf5ff9e286369c7f60f5a04339193736b2f5a3e
Renan Muniz <fmrenan@outlook.com>
2021-06-21 20:57:14 -0300

    SQL seed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,7 +7,7 @@ INSERT INTO specialties (name) VALUES('Cirurgia cabeça e pescoço');
 INSERT INTO specialties (name) VALUES('Cirurgia cardíaca');
 INSERT INTO specialties (name) VALUES('Cirurgia de tórax');
 
-INSERT INTO adresses (id, bairro, cep, complemento, gia, ibge, localidade, logradouro, siafi, uf) VALUES (1, 'Jardim Helena', '08090284', '(Ch Três Meninas)', '1004', '3550308', 'São Paulo', 'Rua 03 de Outubro', '7107', 'SP');
+INSERT INTO adresses (bairro, cep, complemento, gia, ibge, localidade, logradouro, siafi, uf) VALUES ('Jardim Helena', '08090284', '(Ch Três Meninas)', '1004', '3550308', 'São Paulo', 'Rua 03 de Outubro', '7107', 'SP');
 
 INSERT INTO doctors (active, cell_phone, crm, name, phone, address_id) VALUES ('TRUE', '11988100022', '1234567', 'José Silva Ferreira', '1130254013', 1);
 
